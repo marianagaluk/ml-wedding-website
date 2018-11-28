4fb2802a228d7edb55535b153aa2d5d12aa075e2
Mariana Galuk <mbianchi@tcd.ie>
2018-11-28 23:25:30 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onscroll = function() { myFunction() };
+
+var navbar = document.getElementById( "navbar" );
+var sticky = navbar.offsetTop;
+var height = navber.offsetHeight + "px";
+
+function myFunction() {
+  if ( window.pageYOffset > sticky ) {
+    navbar.classList.add( "sticky" );
+  } else {
+    navbar.classList.remove( "sticky" );
+  }
+}
+
+//fazer um botao back to top ou algo assim

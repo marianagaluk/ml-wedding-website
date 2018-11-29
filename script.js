4a1d90f1e6db3193172cd22edda8bc260b98b272
Mariana Galuk <mbianchi@tcd.ie>
2018-11-29 01:48:02 +0000

    media queries

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ window.onscroll = function() { myFunction() };
 
 var navbar = document.getElementById( "navbar" );
 var sticky = navbar.offsetTop;
-var height = navber.offsetHeight + "px";
+var height = navbar.offsetHeight + "px";
 
 function myFunction() {
   if ( window.pageYOffset > sticky ) {
     navbar.classList.add( "sticky" );
+    // var w = window.innerWidth;
+    // navbar.style.width = w + "px";
   } else {
     navbar.classList.remove( "sticky" );
   }

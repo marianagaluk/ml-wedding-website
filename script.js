4b9ee1e68bd8b84af25439d2c3f33f384cbaed26
Mariana Galuk <mbianchi@tcd.ie>
2018-12-07 13:25:47 +0000

    fixed bugs, changed navbar

--- script.js
+++ script.js
@@ -1,17 +1,87 @@
-//sticky navbar
 
-window.onscroll = function() { myFunction() };
+var winWidth = window.outerWidth;
+console.log('winWidth is ' + winWidth);
+
+window.onload = function() { getInitialSize() };
+
+//sticky navbar
+window.onscroll = function() { stickyNavbar() };
 
 var navbar = document.getElementById( "navbar" );
 var sticky = navbar.offsetTop;
 var height = navbar.offsetHeight + "px";
 
-function myFunction() {
-  if ( window.pageYOffset > sticky ) {
-    navbar.classList.add( "sticky" );
-    // var w = window.innerWidth;
-    // navbar.style.width = w + "px";
-  } else {
-    navbar.classList.remove( "sticky" );
+//onclick event to display the hidden div
+function hideUnhide() {
+  var x = document.getElementById("gift-options");
+      if (x.style.display == "none" || x.style.display == "") {
+          x.style.display = "block";
+          location.href='#gift-options';
+      } else {
+          x.style.display = "none";
+      }
+  }
+
+function getInitialSize() {
+  if (winWidth >= 1400) {
+    toggleContainer();
   }
 }
+
+function stickyNavbar() {
+  if ( window.pageYOffset > sticky) {
+    navbar.classList.add( 'sticky' );
+    document.body.style.paddingTop = navbar.offsetHeight + 'px';
+
+  } else {
+    document.body.style.paddingTop = 0;
+    navbar.classList.remove( 'sticky' );
+  }
+}
+
+//change toggle from container-fluid to container;
+function toggleContainer() {
+  var section = document.getElementsByClassName('section');
+  for (var i =0; i<section.length; i++){
+    section[i].classList.remove("container-fluid");
+    section[i].classList.add( "container" );
+  }
+}
+
+//change toggle from container to container-fluid;
+function toggleContainerFluid() {
+  var section = document.getElementsByClassName('section');
+  for (var i =0; i<section.length; i++){
+    section[i].classList.remove("container");
+    section[i].classList.add( "container-fluid" );
+  }
+}
+
+
+//function to get the size of the window every time the window is resized
+function getSize() {
+  var w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName('body')[0],
+      x = w.outerWidth;
+      y = w.outerHeight;
+      console.log(x + ' × ' + y);
+
+//verify if window is bigger than 1440px
+    if (x >= 1440){
+      toggleContainer();
+    } else {
+      toggleContainerFluid();
+    }
+
+}
+
+
+
+
+
+
+
+// -----------------------------------------------------------------------------
+//Button lista de presentes
